16a50b087d61fc7eb36a999cd3836fa23f831923
Danarki <daan-alexander@hotmail.com>
2022-04-19 13:12:02 +0200

    Added LED functions for init and turning on LEDs

--- projects/Parking Monitor/src/main.c
+++ projects/Parking Monitor/src/main.c
@@ -1,7 +1,17 @@
 #include "stm32f0xx.h"
 #include "stm32f0_discovery.h"
+#include "stdbool.h"
+#include "led_functions.h"
+
+void delay(const int d);
 
 int main(){
+	init_LED_GPIO();
+
+	while(true){
+		updateLEDS(1);
+	}
+	
 	return 0;
 }
 
